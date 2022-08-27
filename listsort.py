1c7a8dfc0552d86cc6d3dae19c5b46353f8e4792
8A <32315516+Irlanda3@users.noreply.github.com>
2022-08-26 20:28:43 -0600

    Add files via upload
    sort by alphabet

---
+++ listsort.py
@@ -0,0 +1,9 @@
+thislist = ['mangos', 'papaya', 'melon', 'sandia']
+print(thislist)
+thislist.sort()
+print(thislist)
+
+#coprehension list
+
+newlist = [x for x in thislist if 'a' in x]
+print(newlist)
