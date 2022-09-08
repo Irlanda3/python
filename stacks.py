f74f837864cb7526e293c7dc77c4f5781cce6b46
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-07 21:11:51 -0600

    Add files via upload

---
+++ stacks.py
@@ -0,0 +1,18 @@
+from collections import deque
+myStack = deque()
+
+myStack.append('a')
+myStack.append('b')
+myStack.append('c')
+
+myStack
+
+
+myStack.pop()
+
+myStack.pop()
+
+myStack.pop()
+
+
+myStack.pop()
