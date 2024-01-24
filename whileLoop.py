6a990678f6e9757794a6f09a958e0d3ba38a80f7
8A <32315516+Irlanda3@users.noreply.github.com>
2024-01-23 22:26:56 -0700

    Create whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,12 @@
+# While loop
+#While loop syntax 
+#A while loop is a control structure that allows you to repeatedly execute a block of code for as long as a certain condition is true. 
+
+while condition:
+   # Code block to execute
+
+#--- example----
+x = 1
+while x < 100:
+   print(x)
+   x = x*2
